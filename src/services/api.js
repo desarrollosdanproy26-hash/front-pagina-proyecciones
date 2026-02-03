8359bad040b91e8c05d7eb9ff4609467895b22a0
juniorr7b <juniorrivera028@gmail.com>
2026-02-03 00:46:28 -0500

    puerto5000

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 //const API_URL = 'http://localhost:5000/api';
-const API_URL='http://147.93.190.116:500/api'
+const API_URL='http://147.93.190.116:5000/api'
 
 const apiClient = axios.create({
   baseURL: API_URL,
